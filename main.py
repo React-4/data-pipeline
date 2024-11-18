10617e1f4fee8116f1442a25ee01a8d74d3c359f
ham <tomy8964@naver.com>
2024-11-18 15:34:40 +0900

    KAN-15--feat:현재 실시간 모든 종목 현재가, 등락율, 등락금액 크롤링
    - 테이블
        ```python
        "ticker": "Int"
        "currentPrice": float,
        "changeRate": float,
        "changeAmount": float
        ```
    - 수집 방법
        - 다음 증권 크롤링
    - 업데이트 주기
        - 5초마다 수집하여 `Redis` 저장

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import price_crawler
+import currentPrice
 
 df = pd.read_csv("data/listed_companies.csv", dtype={'종목코드': str})
 df = df[["회사명","종목코드"]].reset_index().rename(columns={"index": "stock_id"})
 
 pc = price_crawler.stock_price_crawler(df)
+cp = currentPrice()
